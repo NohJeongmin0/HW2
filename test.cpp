48227c2e360253d3a3d244bc9afadf8e01ecc620
NohJeongmin0 <njm7918@yonsei.ac.kr>
2025-10-26 10:53:07 +0900

    Implement main function in test.cpp
    Add a toy example to demonstrate convertQuaternary usage.

---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include "ConvertQuaternary.hpp"
+#include <iostream>
+
+int main() {
+    // A toy example. Feel free to play around with this file.
+    std::cout << convertQuaternary(12322233, false) << std::endl;
+    std::cout << convertQuaternary(12322233, true) << std::endl;
+
+    return 0;
+}
