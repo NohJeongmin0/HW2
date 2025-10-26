36288558d376cc725a3f0f778b7ae60c0849bdb2
NohJeongmin0 <njm7918@yonsei.ac.kr>
2025-10-26 10:53:26 +0900

    Add header file for ConverQuaternary

---
+++ ConverQuaternary.hpp
@@ -0,0 +1,21 @@
+#ifndef CONVERTQUATERNARY_HPP
+#define CONVERTQUATERNARY_HPP
+
+// TODO: Include any additional headers you may need here.
+// iostream and string are already included for you.
+#include <iostream>
+#include <string>
+
+/*****************************************
+YOU MUST EDIT THE STUDENT ID BELOW!!!
+******************************************/
+const std::string studentID = "1234567890";
+/*****************************************
+YOU MUST EDIT THE STUDENT ID ABOVE!!!
+******************************************/
+
+int convertQuaternary(int inputNum, bool inputType); // The function you will need to implement
+
+// TODO: If you need additional helper functions, you can define them here
+
+#endif // CONVERTQUATERNARY_HPP
